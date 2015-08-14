cc154c1eae0fca020edbcc643f1a0f5bd036b672
benburkett11 <benburkett11@gmail.com>
2015-08-14 02:27:14 -0400

    Added detail functionality

---
+++ vendor/constants.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ben
+ * Date: 8/14/2015
+ * Time: 12:52 AM
+ */
+
+define( 'PRODUCTDETIALCLASS', 'product-details' );
+define( 'PRODUCTPRICECLASS', 'product-to-cart' );
+define( 'COUNTCLASS', 'count' );
+define( 'PAGINATIONCLASS', 'pagination' );
+define( 'BODYBUILDING', 'http://www.bodybuilding.com' );
