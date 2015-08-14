5cea76c285e9c54e424fff221a0fbc4d665b1342
benburkett11 <benburkett11@gmail.com>
2015-08-14 02:25:31 -0400

    Added detail functionality

--- curl.php
+++ curl.php
@@ -10,23 +10,23 @@ libxml_use_internal_errors(true);
 set_time_limit( 300 );
 $time_start = microtime(true);
 $recordCount = 0;
-
 use \Curl\Curl;
 
-$baseURL = 'http://www.bodybuilding.com';
 $curl = new Curl();
 $curl->get( 'http://www.bodybuilding.com/store/whey.html' );
 $content = $curl->response;
 
 htmlentities( $content );
+$content = iconv( 'UTF-8', 'UTF-8//TRANSLIT', $content );
+//$content = preg_replace('~\p{C}+~u', '', $content);
+//echo $content;
+//exit;
 $dom = new DOMDocument();
 $dom->loadHTML( $content );
 $selector = new DOMXPath($dom);
 
-$countClass = 'count';
-$paginationClass = 'pagination';
-$resultClass = $selector->query("//*[contains(concat(' ', normalize-space(@class), ' '), ' $countClass ')]");
-$link = $selector->query("//*[contains(concat(' ', normalize-space(@class), ' '), ' $paginationClass ')]");
+$resultClass = $selector->query("//*[contains(concat(' ', normalize-space(@class), ' '), ' " . COUNTCLASS . " ')]");
+$link = $selector->query("//*[contains(concat(' ', normalize-space(@class), ' '), ' " . PAGINATIONCLASS . " ')]");
 
 $linkArray = getPageLink( $dom->saveXML( $link[0] ) );
 
@@ -40,14 +40,18 @@ $paramFirst = $linkArray['link'] . '?' . $linkArray[1][0] . '=';
 $paramSecond = '&'. $linkArray[2][0] . '=50&' . $linkArray[3][0] . '=';
 
 for( $i = 1; $i <= $count; $i++ ){
-    $curl->get( $baseURL . $paramFirst . $i . $paramSecond . $count );
+    $curl->get( BODYBUILDING . $paramFirst . $i . $paramSecond . $count );
     $dom->loadHTML( $curl->response );
-    $tempSelector = new DOMXPath($dom);
-    $articleTag = $tempSelector->query('/html/body//article');
+    $tempSelector = new DOMXPath( $dom );
+    $articleTag = $tempSelector->query( '/html/body//article' );
     foreach( $articleTag as $ar ){
-        echo $ar->nodeValue . '<br/><br/>';
+        $details = parseContent( $dom->saveXML( $ar ), PRODUCTDETIALCLASS );
+        print_r( $details );
+        //echo $ar->nodeValue . '<br/><br/>';
         $recordCount++;
+        exit;
     }
+
 }
 
 $curl->close();
@@ -56,13 +60,43 @@ $execution_time = ($time_end - $time_start);
 echo '<b>Total Execution Time:</b> '.$execution_time.' Mins<br/>';
 echo 'Record count: ' . $recordCount;
 
+function parseContent( $html, $tag ){
+    $tempDom = new DOMDocument();
+    $tempDom->loadHTML( $html );
+    $xPath = new DOMXPath( $tempDom );
+    $token = $xPath->query("//*[contains(concat(' ', normalize-space(@class), ' '), ' $tag ')]");
+
+    switch( $tag ){
+        case PRODUCTDETIALCLASS:
+            $title = $xPath->query("//h3");
+            $details['title'] = preg_replace('/[^0-9a-zA-Z]/', "", $title[0]->nodeValue ) . '<br/>';
+            $detailList = $xPath->query("//ul/li");
+            foreach( $detailList as $d ){
+                $details[] = preg_replace('/[^0-9a-zA-Z.:%_\s]/', "", $d->nodeValue ) . '<br/>';
+            }
+            break;
+    }
+
+    return $details;
+
+    /*
+    $all = $xPath->query("//div");
+    foreach( $all as $t ){
+        $block = $tempDom->saveXML( $t );
+        echo $block;
+    }
+    */
+}
 
 function getPageLink( $html ){
     $tempDom = new DOMDocument();
     $tempDom->loadHTML( $html );
     $xPath = new DOMXPath( $tempDom );
     $liTag = $xPath->query('//a');
-
+    /*
+    foreach( $liTag as $li ){
+        echo $li->nodeValue;
+    }*/
     $link = ( $liTag[0]->getAttribute( 'href' ) == '#' ) ? $liTag[1]->getAttribute( 'href' ) : 'error' ;
 
     if( $link == 'error' ){
